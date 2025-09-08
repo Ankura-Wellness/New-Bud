6aab19748ba6e201abb21f7241b3c14dab52c613
Vassudeo Shetye <vassudeo.shetye@binarydharma.online>
2025-09-08 21:20:05 +0530

    Replaced Banner

--- front-page.php
+++ front-page.php
@@ -8,7 +8,7 @@ $base_url = home_url();
 get_header(null,['title'=>'Home']);
 ?>
 <div id="nbFrontPage">
-    <img class="w-100 d-none d-md-block" src="http://ankurah.com/wp-content/uploads/2025/04/banner_2.png" alt="">
+    <img class="w-100 d-none d-md-block" src="http://ankurah.com/wp-content/uploads/2025/04/banner_3.png" alt="">
     <img class="w-100 d-block d-md-none" src="<?php echo esc_url( get_template_directory_uri() . '/assets/src/img/front-page/banner_2_m.png' ); ?>" alt="">
     <div id="container">
     <div id="box">
@@ -48,85 +48,86 @@ get_header(null,['title'=>'Home']);
             </div>
         </div>
     </section>
-    <section id="nbProductsIntro" class="container my-5">
-        <div class="row justify-content-center">
-            <div class="col-12 d-flex justify-content-center mb-4">
-                <h2 class=" text-center mb-4">
-                    Our Products
-                </h2>
-            </div>
-        </div>
-        <div class="row justify-content-center align-items-center mb-4">
-            <div class="col-12 col-md-5 mb-4">
-                <img src="https://ankurah.com/wp-content/uploads/2025/04/c43b74b8-5ee8-4418-9476-e24a5cd9eba5.jpeg" style="width: 100%" alt="">
-            </div>
-            <div class="col-12 col-md-5 mb-4">
-                <h3 class="text-center primaryFont" style="font-weight: 800; font-size: 2.5rem; margin-top: 1rem;">
-                    Dried Banana Munchies
-                </h3>
-                <h5 class="text-center heroFont">
-                    Fuel your day with nature's sweetness, one bite at a time
-                </h5>
-                <p>
-                    Indulge in the pure goodness of nature with <b>Dried Banana Munchies</b> 100% natural, sun-dried, and free from added sugars, preservatives, or chemicals. Handpicked from lush organic farms, our bananas are grown without fertilizers or pesticides, ensuring the highest quality and rich nutritional benefits in every bite
-                </p>
-                <div id="nbFeatures" class="d-flex justify-content-center">
-                    <h6 class="heroFont">
-                        Why have Munchies?
-                    </h6>
-                    <ul>
-                        <li>
-                            Rich in Potassium – Supports heart health & muscle function
-                        <li>
-                            Packed with Fiber – Aids digestion & gut health
-                        </li>
-                        <li>
-                            High in Natural Energy – Perfect for quick snacks & workouts
-                        </li>
-                        <li>
-                            Loaded with Antioxidants – Helps boost immunity & fight free radicals
-                        </li>
-                    </ul>
+    <section id="nbProductsIntro" class="container-fluid my-5">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 d-flex justify-content-center mt-5 mb-3">
+                    <h2 class=" text-center mb-4">
+                        Our Products
+                    </h2>
                 </div>
             </div>
-        </div>
-        <div class="row justify-content-center align-items-center">
-            <div class="col-12 col-md-5 mb-4 d-block d-md-none">
-                <img src="https://ankurah.com/wp-content/uploads/2025/04/a3e42672-6457-4ffb-9b15-fe2ba2b8c524-e1744962262489.jpeg" style="width: 100%" alt="">
-            </div>
-            <div class="col-12 col-md-5 mb-4">
-                <h3 class="text-center primaryFont" style="font-weight: 800; font-size: 2.5rem; margin-top: 1rem;">
-                    Goan Dried Coconut
-                </h3>
-                <h5 class="text-center heroFont">
-                    Enjoy the goodness of pure Goan Coconut in every dish, naturally Fresh, naturally Delicious!
-                </h5>
-                <p>
-                    Sourced from the lush coconut groves of Goa, our <b>Goan Dried Coconut</b> is 100% organic, grown without fertilizers or pesticides, and dried naturally to retain its rich flavor, aroma, and nutrients.
-                </p>
-                <div id="nbFeatures" class="d-flex justify-content-center">
-                    <h6 class="heroFont">
-                        Versatile & Easy to Use
-                    </h6>
-                    <ul>
-                        <li>
-                            Perfect for coconut-based curries – Adds richness & thickness
-                        </li>
-                        <li>
-                            Ideal for traditional Goan sweets – Enhances authentic taste & texture
-                        </li>
-                        <li>
-                            Convenient & long shelf life – No grating or grinding needed
-                        </li>
-                    </ul>
+            <div class="row justify-content-center align-items-center mb-4">
+                <div class="col-12 col-sm-6 col-md-5 mb-4">
+                    <img src="https://ankurah.com/wp-content/uploads/2025/04/c43b74b8-5ee8-4418-9476-e24a5cd9eba5.jpeg" style="width: 100%" alt="">
+                </div>
+                <div class="col-12 col-md-5 mb-4">
+                    <h3 class="text-center primaryFont" style="font-weight: 800; font-size: 2.5rem; margin-top: 1rem;">
+                        Dried Banana Munchies
+                    </h3>
+                    <h5 class="text-center heroFont">
+                        Fuel your day with nature's sweetness, one bite at a time
+                    </h5>
+                    <p>
+                        Indulge in the pure goodness of nature with <b>Dried Banana Munchies</b> 100% natural, sun-dried, and free from added sugars, preservatives, or chemicals. Handpicked from lush organic farms, our bananas are grown without fertilizers or pesticides, ensuring the highest quality and rich nutritional benefits in every bite
+                    </p>
+                    <div id="nbFeatures" class="d-flex justify-content-center">
+                        <h6 class="heroFont">
+                            Why have Munchies?
+                        </h6>
+                        <ul>
+                            <li>
+                                Rich in Potassium – Supports heart health & muscle function
+                            <li>
+                                Packed with Fiber – Aids digestion & gut health
+                            </li>
+                            <li>
+                                High in Natural Energy – Perfect for quick snacks & workouts
+                            </li>
+                            <li>
+                                Loaded with Antioxidants – Helps boost immunity & fight free radicals
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-            <div class="col-12 col-md-5 mb-4 d-none d-md-block">
-                <img src="https://ankurah.com/wp-content/uploads/2025/04/a3e42672-6457-4ffb-9b15-fe2ba2b8c524-e1744962262489.jpeg" style="width: 100%" alt="">
+            <div class="row justify-content-center align-items-center">
+                <div class="col-12  col-sm-6 col-md-5 mb-4 d-block d-md-none">
+                    <img src="https://ankurah.com/wp-content/uploads/2025/04/a3e42672-6457-4ffb-9b15-fe2ba2b8c524-e1744962262489.jpeg" style="width: 100%" alt="">
+                </div>
+                <div class="col-12  col-sm-6 col-md-5 mb-4">
+                    <h3 class="text-center primaryFont" style="font-weight: 800; font-size: 2.5rem; margin-top: 1rem;">
+                        Goan Dried Coconut
+                    </h3>
+                    <h5 class="text-center heroFont">
+                        Enjoy the goodness of pure Goan Coconut in every dish, naturally Fresh, naturally Delicious!
+                    </h5>
+                    <p>
+                        Sourced from the lush coconut groves of Goa, our <b>Goan Dried Coconut</b> is 100% organic, grown without fertilizers or pesticides, and dried naturally to retain its rich flavor, aroma, and nutrients.
+                    </p>
+                    <div id="nbFeatures" class="d-flex justify-content-center">
+                        <h6 class="heroFont">
+                            Versatile & Easy to Use
+                        </h6>
+                        <ul>
+                            <li>
+                                Perfect for coconut-based curries – Adds richness & thickness
+                            </li>
+                            <li>
+                                Ideal for traditional Goan sweets – Enhances authentic taste & texture
+                            </li>
+                            <li>
+                                Convenient & long shelf life – No grating or grinding needed
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-12  col-sm-6 col-md-5 mb-4 d-none d-md-block">
+                    <img src="https://ankurah.com/wp-content/uploads/2025/04/a3e42672-6457-4ffb-9b15-fe2ba2b8c524-e1744962262489.jpeg" style="width: 100%" alt="">
+                </div>
             </div>
         </div>
     </section>
 </div>
-
 
 <?php get_footer(null,['title'=>'Home']);?>
