57c2d47381ff10f93f882ff64eb932c8fe89b2a9
Vassudeo Shetye <vassudeo.shetye@binarydharma.online>
2025-09-09 17:38:54 +0530

    About Us Page Fix

--- page-our-story.php
+++ page-our-story.php
@@ -18,8 +18,10 @@ get_header(null,['title'=>'Our Story']);
             <h2 class="col-12 primaryFont text-center my-5 fw-bold">
                 Our Motive
             </h2>
-            <div class="col-12 col-md-5">
-                At <b>Ankuräḫ</b>, we are committed to bringing nature’s purest offerings directly to your kitchen. Our products are rooted in the philosophy of simplicity-untouched, unaltered, and full of natural goodness. We believe that true quality lies in keeping things as close to their original state as possible. That’s why each item we provide is carefully sourced and minimally handled, ensuring that you receive only the best of what nature has to offer.
+            <div class="col-12">
+                <p id="nbFirstParagraph" class="text-left">
+                    At <b>Ankuräḫ</b>, we are committed to bringing nature’s purest offerings directly to your kitchen. Our products are rooted in the philosophy of simplicity-untouched, unaltered, and full of natural goodness. We believe that true quality lies in keeping things as close to their original state as possible. That’s why each item we provide is carefully sourced and minimally handled, ensuring that you receive only the best of what nature has to offer.
+                </p>
             </div>
         </div>
     </div>
