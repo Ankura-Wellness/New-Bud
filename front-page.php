5c492929f94f8a0e724bca2f089214139d200560
Vassudeo Shetye <vassudeo.shetye@binarydharma.online>
2025-09-12 12:03:24 +0530

    Removed Buy Now

--- front-page.php
+++ front-page.php
@@ -59,7 +59,7 @@ get_header(null,['title'=>'Home']);
             </div>
             <div class="row justify-content-center align-items-center mb-4">
                 <div class="col-12 col-sm-6 col-md-5 mb-4">
-                    <img src="https://ankurah.com/wp-content/uploads/2025/04/c43b74b8-5ee8-4418-9476-e24a5cd9eba5.jpeg" style="width: 100%" alt="">
+                    <img src="http://ankurah.com/wp-content/uploads/2025/04/banana-packet-with-Sample.png" style="width: 100%" alt="">
                 </div>
                 <div class="col-12 col-md-5 mb-4">
                     <h3 class="text-center primaryFont" style="font-weight: 800; font-size: 2.5rem; margin-top: 1rem;">
@@ -92,9 +92,9 @@ get_header(null,['title'=>'Home']);
                     <a class="btn btn-custom dark" href="https://wa.me/917620805302?text=Hi%2C%20I%20would%20like%20to%20place%20an%20order%20for%20Dried%20Banana%20Munchies">
                         Enquire Now
                     </a>
-                    <a class="btn btn-custom dark" href="http://localhost/ankurah/product/dried-banana-munchies/">
+                    <!-- <a class="btn btn-custom dark" href="http://localhost/ankurah/product/dried-banana-munchies/">
                         Buy Now
-                    </a>
+                    </a> -->
                 </div>
             </div>
             <div class="row justify-content-center align-items-center">
