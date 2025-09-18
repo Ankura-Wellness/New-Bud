7f1f690d8dc9aa25f45918a57f70f0cd33187b7e
Vassudeo Shetye <vassudeo.shetye@binarydharma.online>
2025-09-18 22:29:59 +0530

    Cart Layout Fixed

--- assets/src/scripts/njs/libs/httpManager.njs.js
+++ assets/src/scripts/njs/libs/httpManager.njs.js
@@ -2,8 +2,8 @@ export const httpManager = function ($http) {
    return { 
    		postRequester: function (requestMethod,parameters,secure,successCallback,errorCallback) {
             // var webUrl = process.env.NODE_ENV == 'production' ? 'https://zantyes.com/' : 'https://localhost/zantyes/';
-			// var webUrl = 'https://ankurah.com/';
-			var webUrl = 'http://localhost/ankurah/';
+			var webUrl = 'https://ankurah.com/';
+			// var webUrl = 'http://localhost/ankurah/';
             let params_string = ''
             for(const parameter in parameters ){
                 params_string = params_string.length == 0 ? '' :  params_string + '&';
