9ca634dc28766888eb253d26c5a7293ca2a490d4
Vassudeo Shetye <vassudeo.shetye@binarydharma.online>
2025-09-18 22:42:35 +0530

    Version Update

--- assets/src/scripts/njs/libs/httpManager.njs.js
+++ assets/src/scripts/njs/libs/httpManager.njs.js
@@ -3,7 +3,6 @@ export const httpManager = function ($http) {
    		postRequester: function (requestMethod,parameters,secure,successCallback,errorCallback) {
             // var webUrl = process.env.NODE_ENV == 'production' ? 'https://zantyes.com/' : 'https://localhost/zantyes/';
 			var webUrl = 'https://ankurah.com/';
-			// var webUrl = 'http://localhost/ankurah/';
             let params_string = ''
             for(const parameter in parameters ){
                 params_string = params_string.length == 0 ? '' :  params_string + '&';
