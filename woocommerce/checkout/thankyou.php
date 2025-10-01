4519ccb12e5c8fccf84555dccaa753e657f54ef9
Vassudeo Shetye <vassudeo.shetye@binarydharma.online>
2025-10-01 10:35:39 +0530

    updated thank you page

--- woocommerce/checkout/thankyou.php
+++ woocommerce/checkout/thankyou.php
@@ -11,7 +11,7 @@ $order = new WC_Order($args['order']);
 
 <div class="container" id="bnCheckoutThankYou">
     <div class="row my-5">
-        <div class="col-6 d-flex flex-column align-items-center">
+        <div class="col-12 col-md-6 d-flex flex-column align-items-center justify-content-center">
             <img src="https://ankurah.com/wp-content/uploads/2025/09/front_label_top_it2_2.png" class="mb-3" style="height: 150px" alt="">
             <div class="d-flex align-items-center mb-4">
                 <img src="https://ankurah.com/wp-content/uploads/2025/09/accept.png" style="height: 50px">
@@ -24,33 +24,36 @@ $order = new WC_Order($args['order']);
                 Thank you , <?php echo esc_html($order->get_shipping_first_name()); ?>
             </h3>
 			<p class="secondaryFont mx-2 text-center">
-				Our pride lies in providing our customers with only the best produce and Thank you for being part of our motive.
+				Our pride lies in providing our customers with only the best produce and <br> 
+                <strong>
+                    Appreciate you for being part of our motive
+                </strong>.
 			</p>
         </div>
-        <div class="col-6 row justify-content-center" id="nbInfoArea">
+        <div class="col-12 col-md-6 row justify-content-center" id="nbInfoArea">
             <div class="col-md-3 primaryFont">
                 <span class="fw-bold tex">
                     # Order
                 </span> <br>
-                <small>
+                <p>
                     <?php print_r($order->id); ?>
-                </small>
+                </p>
             </div>
             <div class="col-md-3 primaryFont">
                 <span class="fw-bold">
                     Date
                 </span> <br>
-                <small>
+                <p>
                     <?php print_r($order->get_date_created()->format('Y-m-d H:i')); ?>
-                </small>
+                </p>
             </div>
             <div class="col-md-3 primaryFont">
                 <span class="fw-bold">
                     Total
                 </span> <br>
-                <small>
+                <p>
                     <?php print_r($order->total); ?>
-                </small>
+                </p>
             </div>
             <div class="col-8 mt-3">
                 <table class="table">
@@ -83,22 +86,39 @@ $order = new WC_Order($args['order']);
                     </tbody>
                 </table>
             </div>
-            <div class="col-12">
+            <div class="col-12 row justify-content-center" id="nbAddress">
                 <div class="col-6">
                     <h5 class="heroFont">
                         Shipping address
                     </h5>
                     <p class="primaryFont">
-                        <?php print_r($order->get_shipping_first_name()); ?> <?php print_r($order->get_shipping_last_name()); ?> <br>
+                        <span class="fw-bold fs-6 fst-italic">
+                            <?php print_r($order->get_shipping_first_name()); ?> <?php print_r($order->get_shipping_last_name()); ?>
+                        </span> <br>
                         <?php print_r($order->get_shipping_address_1()); ?> <br>
                         <?php print_r($order->get_shipping_address_2()); ?> <br>
-                        <?php print_r($order->get_shipping_city()); ?>, <?php print_r($order->get_shipping_state()); ?><br>
-                        <?php print_r($order->get_shipping_postcode()); ?>
+                        <strong>
+                            <?php print_r($order->get_shipping_city()); ?>, <?php print_r($order->get_shipping_state()); ?> <?php print_r($order->get_shipping_postcode()); ?>
+                        </strong>
                     </p>
                 </div>
-                <hr style="border: dashed 1px #000">
-                <p>
-                    All rights reserved with ankurah wellness
+                <div class="col-6">
+                    <h5 class="heroFont">
+                        Billing address
+                    </h5>
+                    <p class="primaryFont">
+                        <span class="fw-bold fs-6 fst-italic">
+                            <?php print_r($order->get_billing_first_name()); ?> <?php print_r($order->get_billing_last_name()); ?> <br>
+                        </span>
+                        <?php print_r($order->get_billing_address_1()); ?> <br>
+                        <?php print_r($order->get_billing_address_2()); ?> <br>
+                        <strong>
+                            <?php print_r($order->get_billing_city()); ?>, <?php print_r($order->get_billing_state()); ?> <?php print_r($order->get_billing_postcode()); ?>
+                        </strong>
+                    </p>
+                </div>
+                <p class="text-center text-black-50" style="border:none;border-top: dashed 1px #000; padding-top:15px;font-size:12px;">
+                    All rights reserved with Ankuräḫ
                 </p>
             </div>
         </div>
